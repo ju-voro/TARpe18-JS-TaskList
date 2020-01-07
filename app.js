02a2864d4b7ec5c3345b8f070b274945ac83d924
Opilane TTHK <opilane@tthk.ee>
2020-01-07 10:29:17 +0200

    Finished

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const clearBtn = document.querySelector(".clear-tasks");
 loadEventListeners();
 
 function loadEventListeners(){
+    //Load tasks frol local storage, if there are any
+    document.addEventListener('DOMContentLoaded', getTasks);
     //Add a task event
     form.addEventListener('submit', addTask);
     //Remove task
@@ -15,6 +17,35 @@ function loadEventListeners(){
     clearBtn.addEventListener('click', clearTasks);
     //filter tasks
     filterInput.addEventListener('keyup', filterTasks);
+}
+
+//get tasks from local storage
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+        //create an li element
+        const li = document.createElement('li');
+        //add class name to the li element
+        li.className = 'collection-item';
+        //create a textnode and append it to the li
+        li.appendChild(document.createTextNode(task));
+
+        const link = document.createElement('a');
+        //add a class to the a element
+        link.className = 'delete-item secondary-content';
+        link.innerHTML = 'X';
+        li.appendChild(link);
+        taskList.appendChild(li);
+
+    });
+
+
 }
 
 function addTask(event){
@@ -36,8 +67,7 @@ function addTask(event){
     li.appendChild(link);
     taskList.appendChild(li);
   
-    //store in LocalStorage
-
+    //store in LocalStorage    
     storeInLocalStorage(taskInput.value);
     taskInput.value = '';
     
@@ -64,8 +94,28 @@ function removeTask(event){
     if(event.target.classList.contains('delete-item')){
         if(confirm('Are you sure you want to delete the task?')){
             event.target.parentElement.remove();
+            console.log(event.target.parentElement.textContent);
+            //Remove from local storage
+            removeTaskFromLocalStorage(event.target.parentElement);
         }
     }
+}
+
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent.slice(0, -1) === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function clearTasks(){
